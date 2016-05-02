4588ae137dd1a138014ada97dac8ec19e3b56247
omar <louisli1989@yahoo.com>
2016-05-02 00:08:00 -0500

    update to final version

--- app/src/main/java/omarlee/myweather/MainActivity.java
+++ app/src/main/java/omarlee/myweather/MainActivity.java
@@ -12,25 +12,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.model.LatLng;
 
 import omarlee.weathermap.MapsActivity;
 import omarlee.weathermap.WeatherData;
+import omarlee.weathermap.WeatherService;
 import omarlee.weathermap.WeatherServiceByName;
 import omarlee.weathermap.WeatherServiceListener;
-public class MainActivity extends AppCompatActivity implements WeatherServiceListener,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
-   LatLng sydy = new LatLng(36,43);
-   // ImageView im= (ImageView)findViewById(R.id.imageView);
+public class MainActivity extends AppCompatActivity implements WeatherServiceListener,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,LocationListener{
     protected TextView   t;
     protected TextView   t2;
     protected TextView   t3;
     protected EditText   et;
+    protected ImageView  im;
     protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+    protected LocationRequest mLocationRequest;
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -38,17 +50,16 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceLis
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
-
+        createLocationRequest();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button bt=(Button) findViewById(R.id.button);
+        im=(ImageView)findViewById(R.id.imageView);
         t=(TextView)findViewById(R.id.textView);
         t2=(TextView)findViewById(R.id.textView2);
         t3=(TextView)findViewById(R.id.textView3);
         et=(EditText)findViewById(R.id.editText);
-       // WeatherServiceByName al = new WeatherServiceByName(this);
-         //al.getWeatherByName("qingdao");
         Button bt2=(Button) findViewById(R.id.button2);
 
         bt2.setOnClickListener(new View.OnClickListener() {
@@ -72,11 +83,36 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceLis
         );
 
     }
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
     @Override
     protected void onStart() {
         mGoogleApiClient.connect();
         super.onStart();
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //stop location update
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
     @Override
     protected void onStop() {
         mGoogleApiClient.disconnect();
@@ -86,14 +122,17 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceLis
     public void onConnected(Bundle connectionHint) {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {}
-       Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                != PackageManager.PERMISSION_GRANTED) {}//request  permission
+    mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
         if(mLastLocation!=null) {
-            et.setText(mLastLocation.toString());
-            WeatherServiceByName al = new WeatherServiceByName(MainActivity.this);
-            al.getWeatherByName(et.getText().toString());
+            LatLng mlocation=new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude());
+            WeatherService al = new WeatherService(MainActivity.this);
+            al.getWeather(mlocation);
         }
         else{
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,this);
             et.setText("Dallas");
             WeatherServiceByName al = new WeatherServiceByName(MainActivity.this);
             al.getWeatherByName(et.getText().toString());
@@ -101,6 +140,12 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceLis
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+       mLastLocation=location;
+        WeatherService al = new WeatherService(MainActivity.this);
+        al.getWeather(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude()));
+    }
 
     @Override
     public void onConnectionSuspended(int i) {
@@ -114,6 +159,9 @@ public class MainActivity extends AppCompatActivity implements WeatherServiceLis
 
     @Override
     public void serviceSuccess(WeatherData weatherData) {
+        String mDrawableName = "icon_"+weatherData.getId();
+        int resID = getResources().getIdentifier(mDrawableName , "drawable", getPackageName());
+        im.setImageDrawable(getResources().getDrawable(resID,null));
         t.setText(""+weatherData.getTemp());
         t2.setText(weatherData.getName());
         t3.setText(weatherData.getWeather());
